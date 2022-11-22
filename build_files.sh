5a4ef3a9208d98d24eecfd65b8de9c0580843998
Mohammad Anarul Islam <43848277+mohammadanarul@users.noreply.github.com>
2022-11-22 23:54:11 +0600

    update: build_files,  vercel and settings file updated.

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 echo 'build start'
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py makemigratons
+python3.9 manage.py makemigratoins
 python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput --clear
 echo 'build end'
