1b827ecbca660d2af9a0984c6c160d5cfca2fe36
Mohammad Anarul Islam <43848277+mohammadanarul@users.noreply.github.com>
2022-11-22 23:29:33 +0600

    update: build_files, requimrents and settings fle updated.

--- core/settings.py
+++ core/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "db.kmzznpuwicwncpuwktgz.supabase.co",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "CATWYf5r2!n6njz",
+        "PORT": "5432",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
